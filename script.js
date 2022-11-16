e73e3f5b4527d5c368bef54c91c214cd364318e0
cheyenne.zeugin <e247154@edu.sbl.ch>
2022-11-16 14:42:50 +0100

    funktion der Lehrtaste

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function handleKKeyPress(ev) {
+function handleKeyPress(ev) {
+    if (ev.code === "Space") {
+
+        const player =
+        document.querySelector("#player")
+        player.setAttribute("cx", 100 * Math.random())
+        player.setAttribute("cy", 100 * Math.random())
+    }
 }
 
 
 
-window.onkeydown = handleKKeyPress
+window.onkeydown = handleKeyPress
