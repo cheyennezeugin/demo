ee14edd84c9b6c4785424e472312eafc3e92ee42
cheyenne.zeugin <e247154@edu.sbl.ch>
2022-11-16 14:46:40 +0100

    player wieder zurück nach blau mit einer Taste

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ function handleKeyPress(ev) {
         player.setAttribute("cy", 100 * Math.random())
     } else if (ev.key === "r") {
         player.setAttribute("fill" , "red")
+
+    } else if (ev.key === "g") {
+        player.setAttribute("fill" , "midnightblue")
     }
 }
 
